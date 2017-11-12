58bae2f4ed9435aced82a731b352efcd5836f2fe
Shyam <shyammohankanojia@gmail.com>
2017-11-13 03:13:16 +0530

    added

---
+++ public/scripts.js
@@ -0,0 +1,31 @@
+/*
+* ----------------------------------------------------------------------------------------
+Author       : wordpressboss
+Template Name: Domain For Sale Template
+Version      : 1.0                                          
+* ----------------------------------------------------------------------------------------
+*/
+
+
+(function ($) {
+    'use strict';
+
+    jQuery(document).ready(function () {
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  PRELOADER JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        var prealoaderOption = $(window);
+        prealoaderOption.on("load", function () {
+            var preloader = jQuery('.preloader');
+            var preloaderArea = jQuery('.preloader-area');
+            preloader.fadeOut();
+            preloaderArea.delay(350).fadeOut('slow');
+        });
+
+    });
+
+})(jQuery);
